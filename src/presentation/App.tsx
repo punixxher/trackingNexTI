bc6877ba0e3c78ec46133dea2a1be12ce192083e
Daniel Silva <danielsilvaorrego@gmail.com>
2024-06-03 15:51:38 -0500

    first commit

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,10 +1,13 @@
-import {DefaultTheme, PaperProvider} from 'react-native-paper';
+// App.tsx
 
-import {SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useState, useEffect, useReducer, createContext} from 'react';
+import {Alert, SafeAreaView, AppState} from 'react-native';
+import * as Keychain from 'react-native-keychain';
+import {DefaultTheme, PaperProvider} from 'react-native-paper';
 import {AuthScreen} from './screens/auth/Auth.tsx';
 import {Home} from './screens/home/Home.tsx';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const theme = {
   ...DefaultTheme,
@@ -17,21 +20,120 @@ const theme = {
 
 const Stack = createNativeStackNavigator();
 
-export const App = () => {
+interface AppState {
+  sessionData: any;
+}
+
+// Define the action types
+type Action = {type: 'LOGINUSER'} | {type: 'LOGOUT'};
+
+// Create the initial state
+const initialState: AppState = {
+  sessionData: {
+    user: 'Daniel',
+  },
+};
+
+const appReducer = (
+  state: AppState,
+  action: Action,
+  data: string,
+): AppState => {
+  switch (action.type) {
+    case 'LOGINUSER':
+      return {...state, sessionData: data};
+    case 'LOGOUT':
+      return {...state, sessionData: data};
+    default:
+      return state;
+  }
+};
+
+export const AppContext = createContext<{
+  state: AppState;
+  dispatch: React.Dispatch<Action>;
+}>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+const App: React.FC = () => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    checkLoginStatus(); // Check if user is already logged in when the component mounts
+  }, []);
+
+  const checkLoginStatus = async () => {
+    try {
+      const credentials = await Keychain.getGenericPassword();
+
+      if (credentials && credentials.username && credentials.password) {
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.error('Error checking login status:', error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await Keychain.resetGenericPassword();
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.error('Error logging out:', error);
+      Alert.alert('Error', 'An error occurred while logging out');
+    }
+  };
+
   return (
-    <NavigationContainer>
-      <PaperProvider theme={theme}>
-        <SafeAreaView style={{flex: 1, backgroundColor: '#F2F2F2'}}>
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-            }}>
-            <Stack.Screen name="auth" component={AuthScreen} />
-            <Stack.Screen name="home" component={Home} />
-          </Stack.Navigator>
-        </SafeAreaView>
-      </PaperProvider>
-    </NavigationContainer>
+    <AppContext.Provider value={{state, dispatch}}>
+      <NavigationContainer>
+        <PaperProvider theme={theme}>
+          <SafeAreaView style={{flex: 1, backgroundColor: '#F2F2F2'}}>
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,
+              }}>
+              <Stack.Screen name="auth" component={AuthScreen} />
+                <Stack.Screen name="home" component={Home} />
+
+
+
+            </Stack.Navigator>
+          </SafeAreaView>
+        </PaperProvider>
+      </NavigationContainer>
+    </AppContext.Provider>
+
+    /*
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          {isLoggedIn ? (
+            <>
+              <Text>Welcome, {username}!</Text>
+              <Button title="Logout" onPress={handleLogout} />
+            </>
+          ) : (
+            <>
+              <TextInput
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+                style={{borderWidth: 1, padding: 10, marginBottom: 10}}
+              />
+              <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={{borderWidth: 1, padding: 10, marginBottom: 10}}
+              />
+              <Button title="Login" onPress={handleLogin} />
+            </>
+          )}
+        </View>
+    */
   );
 };
 
--- src/presentation/screens/auth/Auth.tsx
+++ src/presentation/screens/auth/Auth.tsx
@@ -1,10 +1,46 @@
 import {Button, TextInput} from 'react-native-paper';
-import {Image, SafeAreaView, Text, View} from 'react-native';
+import {Alert, Image, SafeAreaView, Text, View} from 'react-native';
+import Keychain from 'react-native-keychain';
+import {useContext, useState} from 'react';
+import {AppContext} from '../../App.tsx';
+
+const useAppContext = () => useContext(AppContext);
 
 export const AuthScreen = ({navigation}: any) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const {state, dispatch} = useAppContext();
+
+  const handleLogin = async () => {
+    try {
+      // Simulate API call to authenticate user
+      const isAuthenticated = await authenticateUser(username, password);
+      setIsLoggedIn(true);
+      navigation.navigate('home', {name: 'Jane'});
+      return
+      if (isAuthenticated) {
+        await Keychain.setGenericPassword(username, password);
+        setIsLoggedIn(true);
+      } else {
+        Alert.alert('Login Failed', 'Invalid username or password');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      Alert.alert('Error', 'An error occurred while logging in');
+    }
+  };
+
+  const authenticateUser = async (username: string, password: string) => {
+    // Simulate API call to authenticate user (replace with your actual authentication logic)
+    return username === 'user' && password === 'password';
+  };
+
   return (
     <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
       <View style={{alignItems: 'center'}}>
+        <Text>{state.sessionData.name}</Text>
         <Image
           source={require('../../../presentation/img/logo1.png')}
           style={{width: 180, height: 148}}
@@ -26,7 +62,7 @@ export const AuthScreen = ({navigation}: any) => {
         <Button
           style={{marginTop: 30, margin: 40}}
           mode="contained"
-          onPress={() => navigation.navigate('home')}>
+          onPress={() => handleLogin()}>
           Ingresar
         </Button>
       </View>
